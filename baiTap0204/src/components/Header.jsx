38fc3f2ae15567682413ee857f65a6804751d588
trgcghai <conghai.tpma@gmail.com>
2025-04-09 09:38:45 +0700

    update minhChung full ui

--- baiTap0204/src/components/Header.jsx
+++ baiTap0204/src/components/Header.jsx
@@ -8,7 +8,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Header = () => {
   return (
     <header className="px-6 py-2 flex items-center justify-between border-b border-gray-300">
-      {/* <p className="text-2xl font-bold text-[#F44B87]">Dashboard</p>
+      <p className="text-2xl font-bold text-[#F44B87]">Dashboard</p>
       <div className=" flex items-center gap-4">
         <div className="relative">
           <input
@@ -33,8 +33,7 @@ const Header = () => {
         <div>
           <img src="/Avatar.png" className="h-12 rounded-lg" alt="" />
         </div>
-      </div> */}
-      header
+      </div>
     </header>
   );
 };
