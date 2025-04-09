69ab518973b124e7565600a65636fa99fdca9e82
trgcghai <conghai.tpma@gmail.com>
2025-04-09 15:15:47 +0700

    create laytou for web, update minhChung

---
+++ baiTap0904/homepage_ai/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import { Search } from "lucide-react";
+
+function Header() {
+  return (
+    <header className="sticky top-0 z-50 bg-white border-b">header</header>
+  );
+}
+
+export default Header;
